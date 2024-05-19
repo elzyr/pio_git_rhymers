c69a7439318116d87433c7951711d9d99b387a7e
Martyna Szymańska <245938@edu.p.lodz.pl>
2024-05-19 13:01:04 +0200

    zmiana w HanoiRhymer.java

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    public static final int rejectedCount = 0;
+    int rejectedCount = 0;
     int totalRejected = rejectedCount;
 
     public int reportRejected() {
